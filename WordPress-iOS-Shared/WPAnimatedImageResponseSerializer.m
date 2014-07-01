dc5d414161a703f65a9bc2115a7e55c10d387e1b
aerych <aerychj@gmail.com>
2014-07-01 15:08:29 -0500

    Don't check twice when once will do.
    This is a small optimization to save a little bit of processing on slower devices.

--- WordPress-iOS-Shared/WPAnimatedImageResponseSerializer.m
+++ WordPress-iOS-Shared/WPAnimatedImageResponseSerializer.m
@@ -23,10 +23,12 @@
 	
 	static NSString* kGifMimeType = @"image/gif";
 	
-	BOOL isNotAGif = ![response.MIMEType isEqualToString:kGifMimeType];
-	BOOL invalidResponse = ![self validateResponse:(NSHTTPURLResponse*)response data:data error:error];
-	BOOL mustBeHandledBySuperclass = invalidResponse || isNotAGif;
-	
+    // if the image is not a GIF
+	BOOL mustBeHandledBySuperclass = ![response.MIMEType isEqualToString:kGifMimeType];
+    if (!mustBeHandledBySuperclass) {
+        mustBeHandledBySuperclass = ![self validateResponse:(NSHTTPURLResponse*)response data:data error:error];
+    }
+
     if (mustBeHandledBySuperclass) {
         image = [super responseObjectForResponse:response
 											data:data
