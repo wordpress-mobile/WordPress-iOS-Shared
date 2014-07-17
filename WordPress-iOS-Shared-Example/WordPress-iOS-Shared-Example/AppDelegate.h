2e8b432b6f42d987f361128438e97c61c048674f
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-07-17 15:54:55 -0700

    Removing license information from example project app delegate.

--- WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/AppDelegate.h
+++ WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/AppDelegate.h
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.h
-//  WordPress-iOS-Shared-Example
-//
-//  Created by Sendhil Panchadsaram on 7/16/14.
-//  Copyright (c) 2014 WordPress. All rights reserved.
-//
-
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
