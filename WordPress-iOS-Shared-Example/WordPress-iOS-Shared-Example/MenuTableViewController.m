590ad8f011700680f64add3ddd0bcc2d49f3612c
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-07-17 15:55:22 -0700

    Removing unnecessary code from MenuTableViewController.m.

--- WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/MenuTableViewController.m
+++ WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/MenuTableViewController.m
@@ -28,13 +28,11 @@
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
 {
-    // Return the number of sections.
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    // Return the number of rows in the section.
     return [self.menuItems count];
 }
 
@@ -52,18 +50,5 @@
         [self performSegueWithIdentifier:@"ColorsSegue" sender:nil];
     }
 }
-
-
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
