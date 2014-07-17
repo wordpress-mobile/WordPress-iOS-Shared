fb908b21ca5d2ef6d41e25269c363e6f143834de
Sendhil Panchadsaram <sendhilp@gmail.com>
2014-07-17 15:50:31 -0700

    Renaming some classes in example project.
    * TitleAndColor -> ColorDetails
    * FontAndAttributes -> FontDetails

--- WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/ColorsTableViewController.m
+++ WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/ColorsTableViewController.m
@@ -7,7 +7,7 @@
 
 @end
 
-@interface TitleAndColor : NSObject
+@interface ColorDetails : NSObject
 
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) UIColor *color;
@@ -16,13 +16,13 @@
 
 @end
 
-@implementation TitleAndColor
+@implementation ColorDetails
 
 + (instancetype)initWithTitle:(NSString *)title andColor:(UIColor *)color {
-    TitleAndColor *titleAndColor = [[[self class] alloc] init];
-    titleAndColor.title = title;
-    titleAndColor.color = color;
-    return titleAndColor;
+    ColorDetails *colorDetails = [[[self class] alloc] init];
+    colorDetails.title = title;
+    colorDetails.color = color;
+    return colorDetails;
 }
 
 @end
@@ -34,30 +34,30 @@
     if (self) {
         self.title = NSLocalizedString(@"Colors", nil);
         self.colors = @[
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"WordPress Blue", nil) andColor:[WPStyleGuide wordPressBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Base Lighter Blue", nil) andColor:[WPStyleGuide baseLighterBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Base Darker Blue", nil) andColor:[WPStyleGuide baseDarkerBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Light Blue", nil) andColor:[WPStyleGuide lightBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"New Kid on the Block Blue", nil) andColor:[WPStyleGuide newKidOnTheBlockBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Midnight Blue", nil) andColor:[WPStyleGuide midnightBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Jazzy Orange", nil) andColor:[WPStyleGuide jazzyOrange]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Fire Orange", nil) andColor:[WPStyleGuide fireOrange]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Big Eddie Grey", nil) andColor:[WPStyleGuide bigEddieGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Little Eddie Grey", nil) andColor:[WPStyleGuide littleEddieGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Whisper Grey", nil) andColor:[WPStyleGuide whisperGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"All T All Shade Grey", nil) andColor:[WPStyleGuide allTAllShadeGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Read Grey", nil) andColor:[WPStyleGuide readGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"It's Everywhere Grey", nil) andColor:[WPStyleGuide itsEverywhereGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Dark As Night Grey", nil) andColor:[WPStyleGuide darkAsNightGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Text Field Placeholder Grey", nil) andColor:[WPStyleGuide textFieldPlaceholderGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Validation Error Red", nil) andColor:[WPStyleGuide validationErrorRed]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Stats Lighter Blue", nil) andColor:[WPStyleGuide statsLighterBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Stats Darker Blue", nil) andColor:[WPStyleGuide statsDarkerBlue]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Table View Action Color", nil) andColor:[WPStyleGuide tableViewActionColor]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Button Action Color", nil) andColor:[WPStyleGuide buttonActionColor]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Keyboard Color", nil) andColor:[WPStyleGuide keyboardColor]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Notifiations Light Grey", nil) andColor:[WPStyleGuide notificationsLightGrey]],
-                        [TitleAndColor initWithTitle:NSLocalizedString(@"Notifiations Dark Grey", nil) andColor:[WPStyleGuide notificationsDarkGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"WordPress Blue", nil) andColor:[WPStyleGuide wordPressBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Base Lighter Blue", nil) andColor:[WPStyleGuide baseLighterBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Base Darker Blue", nil) andColor:[WPStyleGuide baseDarkerBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Light Blue", nil) andColor:[WPStyleGuide lightBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"New Kid on the Block Blue", nil) andColor:[WPStyleGuide newKidOnTheBlockBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Midnight Blue", nil) andColor:[WPStyleGuide midnightBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Jazzy Orange", nil) andColor:[WPStyleGuide jazzyOrange]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Fire Orange", nil) andColor:[WPStyleGuide fireOrange]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Big Eddie Grey", nil) andColor:[WPStyleGuide bigEddieGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Little Eddie Grey", nil) andColor:[WPStyleGuide littleEddieGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Whisper Grey", nil) andColor:[WPStyleGuide whisperGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"All T All Shade Grey", nil) andColor:[WPStyleGuide allTAllShadeGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Read Grey", nil) andColor:[WPStyleGuide readGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"It's Everywhere Grey", nil) andColor:[WPStyleGuide itsEverywhereGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Dark As Night Grey", nil) andColor:[WPStyleGuide darkAsNightGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Text Field Placeholder Grey", nil) andColor:[WPStyleGuide textFieldPlaceholderGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Validation Error Red", nil) andColor:[WPStyleGuide validationErrorRed]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Stats Lighter Blue", nil) andColor:[WPStyleGuide statsLighterBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Stats Darker Blue", nil) andColor:[WPStyleGuide statsDarkerBlue]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Table View Action Color", nil) andColor:[WPStyleGuide tableViewActionColor]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Button Action Color", nil) andColor:[WPStyleGuide buttonActionColor]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Keyboard Color", nil) andColor:[WPStyleGuide keyboardColor]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Notifiations Light Grey", nil) andColor:[WPStyleGuide notificationsLightGrey]],
+                        [ColorDetails initWithTitle:NSLocalizedString(@"Notifiations Dark Grey", nil) andColor:[WPStyleGuide notificationsDarkGrey]],
                         ];
     }
     return self;
@@ -84,9 +84,9 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"CellIdentifier" forIndexPath:indexPath];
-    TitleAndColor *titleAndColor = [self.colors objectAtIndex:indexPath.row];
-    cell.textLabel.text = titleAndColor.title;
-    cell.backgroundColor = titleAndColor.color;
+    ColorDetails *colorDetails = [self.colors objectAtIndex:indexPath.row];
+    cell.textLabel.text = colorDetails.title;
+    cell.backgroundColor = colorDetails.color;
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
 }
--- WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/FontsTableViewController.m
+++ WordPress-iOS-Shared-Example/WordPress-iOS-Shared-Example/FontsTableViewController.m
@@ -8,7 +8,7 @@
 
 @end
 
-@interface FontAndAttributes : NSObject
+@interface FontDetails : NSObject
 
 @property (nonatomic, strong) NSString *title;
 @property (nonatomic, strong) UIFont *font;
@@ -18,14 +18,14 @@
 
 @end
 
-@implementation FontAndAttributes
+@implementation FontDetails
 
 + (instancetype)initWithTitle:(NSString *)title andFont:(UIFont *)font andAttributes:(NSDictionary *)attributes {
-    FontAndAttributes *fontAndAttributes = [[[self class] alloc] init];
-    fontAndAttributes.title = title;
-    fontAndAttributes.font = font;
-    fontAndAttributes.attributes = attributes;
-    return fontAndAttributes;
+    FontDetails *fontDetails = [[[self class] alloc] init];
+    fontDetails.title = title;
+    fontDetails.font = font;
+    fontDetails.attributes = attributes;
+    return fontDetails;
 }
 
 @end
@@ -37,19 +37,19 @@
     if (self) {
         self.title = NSLocalizedString(@"Fonts", nil);
         self.fonts = @[
-                       [FontAndAttributes initWithTitle:@"Large Post Title Font" andFont:[WPStyleGuide largePostTitleFont] andAttributes:[WPStyleGuide largePostTitleAttributes]],
-                       [FontAndAttributes initWithTitle:@"Post Title Font" andFont:[WPStyleGuide postTitleFont] andAttributes:nil],
-                       [FontAndAttributes initWithTitle:@"Post Title Font Bold" andFont:[WPStyleGuide postTitleFontBold] andAttributes:[WPStyleGuide postTitleAttributes]],
-                       [FontAndAttributes initWithTitle:@"Subtitle Font" andFont:[WPStyleGuide subtitleFont] andAttributes:[WPStyleGuide subtitleAttributes]],
-                       [FontAndAttributes initWithTitle:@"Subtitle Font Italic" andFont:[WPStyleGuide subtitleFont] andAttributes:[WPStyleGuide subtitleItalicAttributes]],
-                       [FontAndAttributes initWithTitle:@"Subtitle Font Bold" andFont:[WPStyleGuide subtitleFont] andAttributes:[WPStyleGuide subtitleAttributesBold]],
-                       [FontAndAttributes initWithTitle:@"Label Font" andFont:[WPStyleGuide labelFont] andAttributes:[WPStyleGuide labelAttributes]],
-                       [FontAndAttributes initWithTitle:@"Label Font Normal" andFont:[WPStyleGuide labelFontNormal] andAttributes:[WPStyleGuide labelAttributes]],
-                       [FontAndAttributes initWithTitle:@"Regular Text Font" andFont:[WPStyleGuide regularTextFont] andAttributes:[WPStyleGuide regularTextAttributes]],
-                       [FontAndAttributes initWithTitle:@"Regular Text Bold" andFont:[WPStyleGuide regularTextFontBold] andAttributes:[WPStyleGuide regularTextAttributes]],
-                       [FontAndAttributes initWithTitle:@"Tableview Text Font" andFont:[WPStyleGuide tableviewTextFont] andAttributes:nil],
-                       [FontAndAttributes initWithTitle:@"Tableview Subtitle Font" andFont:[WPStyleGuide tableviewSubtitleFont] andAttributes:nil],
-                       [FontAndAttributes initWithTitle:@"Tableview Section Header Font" andFont:[WPStyleGuide tableviewSectionHeaderFont] andAttributes:nil],
+                       [FontDetails initWithTitle:@"Large Post Title Font" andFont:[WPStyleGuide largePostTitleFont] andAttributes:[WPStyleGuide largePostTitleAttributes]],
+                       [FontDetails initWithTitle:@"Post Title Font" andFont:[WPStyleGuide postTitleFont] andAttributes:nil],
+                       [FontDetails initWithTitle:@"Post Title Font Bold" andFont:[WPStyleGuide postTitleFontBold] andAttributes:[WPStyleGuide postTitleAttributes]],
+                       [FontDetails initWithTitle:@"Subtitle Font" andFont:[WPStyleGuide subtitleFont] andAttributes:[WPStyleGuide subtitleAttributes]],
+                       [FontDetails initWithTitle:@"Subtitle Font Italic" andFont:[WPStyleGuide subtitleFont] andAttributes:[WPStyleGuide subtitleItalicAttributes]],
+                       [FontDetails initWithTitle:@"Subtitle Font Bold" andFont:[WPStyleGuide subtitleFont] andAttributes:[WPStyleGuide subtitleAttributesBold]],
+                       [FontDetails initWithTitle:@"Label Font" andFont:[WPStyleGuide labelFont] andAttributes:[WPStyleGuide labelAttributes]],
+                       [FontDetails initWithTitle:@"Label Font Normal" andFont:[WPStyleGuide labelFontNormal] andAttributes:[WPStyleGuide labelAttributes]],
+                       [FontDetails initWithTitle:@"Regular Text Font" andFont:[WPStyleGuide regularTextFont] andAttributes:[WPStyleGuide regularTextAttributes]],
+                       [FontDetails initWithTitle:@"Regular Text Bold" andFont:[WPStyleGuide regularTextFontBold] andAttributes:[WPStyleGuide regularTextAttributes]],
+                       [FontDetails initWithTitle:@"Tableview Text Font" andFont:[WPStyleGuide tableviewTextFont] andAttributes:nil],
+                       [FontDetails initWithTitle:@"Tableview Subtitle Font" andFont:[WPStyleGuide tableviewSubtitleFont] andAttributes:nil],
+                       [FontDetails initWithTitle:@"Tableview Section Header Font" andFont:[WPStyleGuide tableviewSectionHeaderFont] andAttributes:nil],
                        ];
     }
     return self;
@@ -75,12 +75,12 @@
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
 {
     FontTableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"FontsCell" forIndexPath:indexPath];
-    FontAndAttributes *fontAndAttributes = self.fonts[indexPath.row];
-    cell.title.text = fontAndAttributes.title;
-    if ([fontAndAttributes.attributes count] > 0) {
-        cell.title.attributedText = [[NSAttributedString alloc] initWithString:fontAndAttributes.title attributes:fontAndAttributes.attributes];
+    FontDetails *fontDetails = self.fonts[indexPath.row];
+    cell.title.text = fontDetails.title;
+    if ([fontDetails.attributes count] > 0) {
+        cell.title.attributedText = [[NSAttributedString alloc] initWithString:fontDetails.title attributes:fontDetails.attributes];
     }
-    cell.title.font = fontAndAttributes.font;
+    cell.title.font = fontDetails.font;
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
     return cell;
 }
