ec677e8bd93b236fe7da418212814018935a903d
Brad Angelcyk <brad@angelcyk.com>
2014-08-13 10:31:51 -0400

    adds new colors for NUX to WPStyleGuide

---
+++ WordPress-iOS-Shared/WPStyleGuide.swift
@@ -0,0 +1,11 @@
+import UIKit
+
+class WPStyleGuide: NSObject {
+    func nuxFormText() -> UIColor {
+        return UIColor(red: CGFloat(50/255), green: CGFloat(65/255), blue: CGFloat(85/255), alpha: CGFloat(1.0))
+    }
+
+    func nuxFormPlaceholderText() -> UIColor {
+        return UIColor(red: CGFloat(144/255), green: CGFloat(174/255), blue: CGFloat(194/255), alpha: CGFloat(1.0))
+    }
+}
