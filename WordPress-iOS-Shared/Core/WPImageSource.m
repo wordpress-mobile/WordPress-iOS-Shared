75e6fe5685e3c1f093822fdd4f57c05bcdd44869
Sérgio Estêvão <sergio.estevao@automattic.com>
2016-04-11 14:59:21 +0100

    This hack is no longer needed in the latest version of AFNetworking

--- WordPress-iOS-Shared/Core/WPImageSource.m
+++ WordPress-iOS-Shared/Core/WPImageSource.m
@@ -1,8 +1,6 @@
 #import <AFNetworking/AFNetworking.h>
 #import "WPImageSource.h"
 
-#import "WPAnimatedImageResponseSerializer.h"
-
 NSString * const WPImageSourceErrorDomain = @"WPImageSourceErrorDomain";
 
 @implementation WPImageSource {
@@ -84,7 +82,7 @@ NSString * const WPImageSourceErrorDomain = @"WPImageSourceErrorDomain";
 
 		AFHTTPRequestOperation *operation = [[AFHTTPRequestOperation alloc] initWithRequest:request];
 		
-		operation.responseSerializer = [[WPAnimatedImageResponseSerializer alloc] init];
+		operation.responseSerializer = [[AFImageResponseSerializer alloc] init];
 		operation.responseSerializer.acceptableContentTypes
 			= [operation.responseSerializer.acceptableContentTypes setByAddingObject:@"image/jpg"];
 		
