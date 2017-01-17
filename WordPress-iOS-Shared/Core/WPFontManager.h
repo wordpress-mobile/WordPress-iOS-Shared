8d6d82e5b9a38011ebca6c3126ce3c694f9a3f3a
Jorge Leandro Perez <jorge@lantean.co>
2017-01-17 17:23:39 +0000

    Switching over to NS_ASSUME_NONNULL_BEGIN

--- WordPress-iOS-Shared/Core/WPFontManager.h
+++ WordPress-iOS-Shared/Core/WPFontManager.h
@@ -1,35 +1,40 @@
 #import <UIKit/UIKit.h>
 
+
+NS_ASSUME_NONNULL_BEGIN
+
 @interface WPFontManager : NSObject
 
 /// Loads the OpenSans font family for the life of the current process.
 /// This effectively makes it possible to look this font up using font descriptors.
 ///
 + (void)loadOpenSansFontFamily;
-+ (nonnull UIFont *)openSansLightFontOfSize:(CGFloat)size __deprecated_msg("Use systemLightFontOfSize instead");
-+ (nonnull UIFont *)openSansItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemItalicFontOfSize instead");
-+ (nonnull UIFont *)openSansLightItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemLightItalicFontOfSize instead");
-+ (nonnull UIFont *)openSansBoldFontOfSize:(CGFloat)size __deprecated_msg("Use systemBoldFontOfSize instead");
-+ (nonnull UIFont *)openSansBoldItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemBoldItalicFontOfSize instead");
-+ (nonnull UIFont *)openSansSemiBoldFontOfSize:(CGFloat)size __deprecated_msg("Use systemSemiBoldFontOfSize instead");
-+ (nonnull UIFont *)openSansSemiBoldItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemSemiBoldItalicFontOfSize instead");
-+ (nonnull UIFont *)openSansRegularFontOfSize:(CGFloat)size __deprecated_msg("Use systemRegularFontOfSize instead");
++ (UIFont *)openSansLightFontOfSize:(CGFloat)size __deprecated_msg("Use systemLightFontOfSize instead");
++ (UIFont *)openSansItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemItalicFontOfSize instead");
++ (UIFont *)openSansLightItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemLightItalicFontOfSize instead");
++ (UIFont *)openSansBoldFontOfSize:(CGFloat)size __deprecated_msg("Use systemBoldFontOfSize instead");
++ (UIFont *)openSansBoldItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemBoldItalicFontOfSize instead");
++ (UIFont *)openSansSemiBoldFontOfSize:(CGFloat)size __deprecated_msg("Use systemSemiBoldFontOfSize instead");
++ (UIFont *)openSansSemiBoldItalicFontOfSize:(CGFloat)size __deprecated_msg("Use systemSemiBoldItalicFontOfSize instead");
++ (UIFont *)openSansRegularFontOfSize:(CGFloat)size __deprecated_msg("Use systemRegularFontOfSize instead");
 
-+ (nonnull UIFont *)systemLightFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)systemItalicFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)systemBoldFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)systemSemiBoldFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)systemRegularFontOfSize:(CGFloat)size;
++ (UIFont *)systemLightFontOfSize:(CGFloat)size;
++ (UIFont *)systemItalicFontOfSize:(CGFloat)size;
++ (UIFont *)systemBoldFontOfSize:(CGFloat)size;
++ (UIFont *)systemSemiBoldFontOfSize:(CGFloat)size;
++ (UIFont *)systemRegularFontOfSize:(CGFloat)size;
 
 /// Loads the Merriwheather font family for the life of the current process.
 /// This effectively makes it possible to look this font up using font descriptors.
 ///
 + (void)loadMerriweatherFontFamily;
-+ (nonnull UIFont *)merriweatherBoldFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)merriweatherBoldItalicFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)merriweatherItalicFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)merriweatherLightFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)merriweatherLightItalicFontOfSize:(CGFloat)size;
-+ (nonnull UIFont *)merriweatherRegularFontOfSize:(CGFloat)size;
++ (UIFont *)merriweatherBoldFontOfSize:(CGFloat)size;
++ (UIFont *)merriweatherBoldItalicFontOfSize:(CGFloat)size;
++ (UIFont *)merriweatherItalicFontOfSize:(CGFloat)size;
++ (UIFont *)merriweatherLightFontOfSize:(CGFloat)size;
++ (UIFont *)merriweatherLightItalicFontOfSize:(CGFloat)size;
++ (UIFont *)merriweatherRegularFontOfSize:(CGFloat)size;
 
 @end
+
+NS_ASSUME_NONNULL_END
--- WordPress-iOS-Shared/Core/WPStyleGuide.h
+++ WordPress-iOS-Shared/Core/WPStyleGuide.h
@@ -1,66 +1,69 @@
 #import <UIKit/UIKit.h>
 
+
+NS_ASSUME_NONNULL_BEGIN
+
 @class WPTextFieldTableViewCell;
 @interface WPStyleGuide : NSObject
 
 // Fonts
-+ (nonnull UIFont *)largePostTitleFont;
-+ (nonnull NSDictionary *)largePostTitleAttributes;
-+ (nonnull UIFont *)postTitleFont;
-+ (nonnull UIFont *)postTitleFontBold;
-+ (nonnull NSDictionary *)postTitleAttributes;
-+ (nonnull NSDictionary *)postTitleAttributesBold;
-+ (nonnull UIFont *)subtitleFont;
-+ (nonnull NSDictionary *)subtitleAttributes;
-+ (nonnull UIFont *)subtitleFontItalic;
-+ (nonnull NSDictionary *)subtitleItalicAttributes;
-+ (nonnull UIFont *)subtitleFontBold;
-+ (nonnull NSDictionary *)subtitleAttributesBold;
-+ (nonnull UIFont *)labelFont;
-+ (nonnull UIFont *)labelFontNormal;
-+ (nonnull NSDictionary *)labelAttributes;
-+ (nonnull UIFont *)regularTextFont;
-+ (nonnull UIFont *)regularTextFontSemiBold;
-+ (nonnull UIFont *)regularTextFontBold;
-+ (nonnull NSDictionary *)regularTextAttributes;
-+ (nonnull UIFont *)tableviewTextFont;
-+ (nonnull UIFont *)tableviewSubtitleFont;
-+ (nonnull UIFont *)tableviewSectionHeaderFont;
-+ (nonnull UIFont *)tableviewSectionFooterFont;
++ (UIFont *)largePostTitleFont;
++ (NSDictionary *)largePostTitleAttributes;
++ (UIFont *)postTitleFont;
++ (UIFont *)postTitleFontBold;
++ (NSDictionary *)postTitleAttributes;
++ (NSDictionary *)postTitleAttributesBold;
++ (UIFont *)subtitleFont;
++ (NSDictionary *)subtitleAttributes;
++ (UIFont *)subtitleFontItalic;
++ (NSDictionary *)subtitleItalicAttributes;
++ (UIFont *)subtitleFontBold;
++ (NSDictionary *)subtitleAttributesBold;
++ (UIFont *)labelFont;
++ (UIFont *)labelFontNormal;
++ (NSDictionary *)labelAttributes;
++ (UIFont *)regularTextFont;
++ (UIFont *)regularTextFontSemiBold;
++ (UIFont *)regularTextFontBold;
++ (NSDictionary *)regularTextAttributes;
++ (UIFont *)tableviewTextFont;
++ (UIFont *)tableviewSubtitleFont;
++ (UIFont *)tableviewSectionHeaderFont;
++ (UIFont *)tableviewSectionFooterFont;
 
 // Color
-+ (nonnull UIColor *)wordPressBlue;
-+ (nonnull UIColor *)lightBlue;
-+ (nonnull UIColor *)mediumBlue;
-+ (nonnull UIColor *)darkBlue;
-+ (nonnull UIColor *)grey;
-+ (nonnull UIColor *)lightGrey;
-+ (nonnull UIColor *)greyLighten30;
-+ (nonnull UIColor *)greyLighten20;
-+ (nonnull UIColor *)greyLighten10;
-+ (nonnull UIColor *)greyDarken10;
-+ (nonnull UIColor *)greyDarken20;
-+ (nonnull UIColor *)greyDarken30;
-+ (nonnull UIColor *)darkGrey;
-+ (nonnull UIColor *)jazzyOrange;
-+ (nonnull UIColor *)fireOrange;
-+ (nonnull UIColor *)validGreen;
-+ (nonnull UIColor *)warningYellow;
-+ (nonnull UIColor *)errorRed;
-+ (nonnull UIColor *)alertYellowDark;
-+ (nonnull UIColor *)alertYellowLighter;
-+ (nonnull UIColor *)alertRedDarker;
++ (UIColor *)wordPressBlue;
++ (UIColor *)lightBlue;
++ (UIColor *)mediumBlue;
++ (UIColor *)darkBlue;
++ (UIColor *)grey;
++ (UIColor *)lightGrey;
++ (UIColor *)greyLighten30;
++ (UIColor *)greyLighten20;
++ (UIColor *)greyLighten10;
++ (UIColor *)greyDarken10;
++ (UIColor *)greyDarken20;
++ (UIColor *)greyDarken30;
++ (UIColor *)darkGrey;
++ (UIColor *)jazzyOrange;
++ (UIColor *)fireOrange;
++ (UIColor *)validGreen;
++ (UIColor *)warningYellow;
++ (UIColor *)errorRed;
++ (UIColor *)alertYellowDark;
++ (UIColor *)alertYellowLighter;
++ (UIColor *)alertRedDarker;
 
 // Misc
-+ (nonnull UIColor *)keyboardColor;
-+ (nonnull UIColor *)textFieldPlaceholderGrey;
-+ (nonnull UIColor *)tableViewActionColor;
++ (UIColor *)keyboardColor;
++ (UIColor *)textFieldPlaceholderGrey;
++ (UIColor *)tableViewActionColor;
 
 // Bar Button Styles
 + (UIBarButtonItemStyle)barButtonStyleForDone;
 + (UIBarButtonItemStyle)barButtonStyleForBordered;
-+ (void)setLeftBarButtonItemWithCorrectSpacing:(nonnull UIBarButtonItem *)barButtonItem forNavigationItem:(nonnull UINavigationItem *)navigationItem;
-+ (void)setRightBarButtonItemWithCorrectSpacing:(nonnull UIBarButtonItem *)barButtonItem forNavigationItem:(nonnull UINavigationItem *)navigationItem;
++ (void)setLeftBarButtonItemWithCorrectSpacing:(UIBarButtonItem *)barButtonItem forNavigationItem:(UINavigationItem *)navigationItem;
++ (void)setRightBarButtonItemWithCorrectSpacing:(UIBarButtonItem *)barButtonItem forNavigationItem:(UINavigationItem *)navigationItem;
 
 // View and TableView Helpers
 + (void)configureColorsForView:(nullable UIView *)view andTableView:(nullable UITableView *)tableView;
@@ -74,23 +77,25 @@
 + (void)configureTableViewSectionFooter:(nullable UIView *)footer;
 
 // Move to a feature category
-+ (nonnull UIColor *)buttonActionColor;
-+ (nonnull UIColor *)nuxFormText;
-+ (nonnull UIColor *)nuxFormPlaceholderText;
++ (UIColor *)buttonActionColor;
++ (UIColor *)nuxFormText;
++ (UIColor *)nuxFormPlaceholderText;
 + (void)configureFollowButton:(nullable UIButton *)followButton;
 
 // Deprecated Colors
-+ (nonnull UIColor *)baseLighterBlue;
-+ (nonnull UIColor *)baseDarkerBlue;
-+ (nonnull UIColor *)newKidOnTheBlockBlue;
-+ (nonnull UIColor *)midnightBlue;
-+ (nonnull UIColor *)bigEddieGrey;
-+ (nonnull UIColor *)littleEddieGrey;
-+ (nonnull UIColor *)whisperGrey;
-+ (nonnull UIColor *)allTAllShadeGrey;
-+ (nonnull UIColor *)readGrey;
-+ (nonnull UIColor *)itsEverywhereGrey;
-+ (nonnull UIColor *)darkAsNightGrey;
-+ (nonnull UIColor *)validationErrorRed;
++ (UIColor *)baseLighterBlue;
++ (UIColor *)baseDarkerBlue;
++ (UIColor *)newKidOnTheBlockBlue;
++ (UIColor *)midnightBlue;
++ (UIColor *)bigEddieGrey;
++ (UIColor *)littleEddieGrey;
++ (UIColor *)whisperGrey;
++ (UIColor *)allTAllShadeGrey;
++ (UIColor *)readGrey;
++ (UIColor *)itsEverywhereGrey;
++ (UIColor *)darkAsNightGrey;
++ (UIColor *)validationErrorRed;
 
 @end
+
+NS_ASSUME_NONNULL_END
