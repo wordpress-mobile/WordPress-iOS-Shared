d9c1bdb0cf23bcef8af67934900f18c150c8b842
Jorge Leandro Perez <jorge@lantean.co>
2017-01-17 13:38:19 +0000

    WPStyleGuide: Nullability

--- WordPress-iOS-Shared/Core/WPStyleGuide.h
+++ WordPress-iOS-Shared/Core/WPStyleGuide.h
@@ -4,93 +4,93 @@
 @interface WPStyleGuide : NSObject
 
 // Fonts
-+ (UIFont *)largePostTitleFont;
-+ (NSDictionary *)largePostTitleAttributes;
-+ (UIFont *)postTitleFont;
-+ (UIFont *)postTitleFontBold;
-+ (NSDictionary *)postTitleAttributes;
-+ (NSDictionary *)postTitleAttributesBold;
-+ (UIFont *)subtitleFont;
-+ (NSDictionary *)subtitleAttributes;
-+ (UIFont *)subtitleFontItalic;
-+ (NSDictionary *)subtitleItalicAttributes;
-+ (UIFont *)subtitleFontBold;
-+ (NSDictionary *)subtitleAttributesBold;
-+ (UIFont *)labelFont;
-+ (UIFont *)labelFontNormal;
-+ (NSDictionary *)labelAttributes;
-+ (UIFont *)regularTextFont;
-+ (UIFont *)regularTextFontSemiBold;
-+ (UIFont *)regularTextFontBold;
-+ (NSDictionary *)regularTextAttributes;
-+ (UIFont *)tableviewTextFont;
-+ (UIFont *)tableviewSubtitleFont;
-+ (UIFont *)tableviewSectionHeaderFont;
-+ (UIFont *)tableviewSectionFooterFont;
++ (nonnull UIFont *)largePostTitleFont;
++ (nonnull NSDictionary *)largePostTitleAttributes;
++ (nonnull UIFont *)postTitleFont;
++ (nonnull UIFont *)postTitleFontBold;
++ (nonnull NSDictionary *)postTitleAttributes;
++ (nonnull NSDictionary *)postTitleAttributesBold;
++ (nonnull UIFont *)subtitleFont;
++ (nonnull NSDictionary *)subtitleAttributes;
++ (nonnull UIFont *)subtitleFontItalic;
++ (nonnull NSDictionary *)subtitleItalicAttributes;
++ (nonnull UIFont *)subtitleFontBold;
++ (nonnull NSDictionary *)subtitleAttributesBold;
++ (nonnull UIFont *)labelFont;
++ (nonnull UIFont *)labelFontNormal;
++ (nonnull NSDictionary *)labelAttributes;
++ (nonnull UIFont *)regularTextFont;
++ (nonnull UIFont *)regularTextFontSemiBold;
++ (nonnull UIFont *)regularTextFontBold;
++ (nonnull NSDictionary *)regularTextAttributes;
++ (nonnull UIFont *)tableviewTextFont;
++ (nonnull UIFont *)tableviewSubtitleFont;
++ (nonnull UIFont *)tableviewSectionHeaderFont;
++ (nonnull UIFont *)tableviewSectionFooterFont;
 
 // Color
-+ (UIColor *)wordPressBlue;
-+ (UIColor *)lightBlue;
-+ (UIColor *)mediumBlue;
-+ (UIColor *)darkBlue;
-+ (UIColor *)grey;
-+ (UIColor *)lightGrey;
-+ (UIColor *)greyLighten30;
-+ (UIColor *)greyLighten20;
-+ (UIColor *)greyLighten10;
-+ (UIColor *)greyDarken10;
-+ (UIColor *)greyDarken20;
-+ (UIColor *)greyDarken30;
-+ (UIColor *)darkGrey;
-+ (UIColor *)jazzyOrange;
-+ (UIColor *)fireOrange;
-+ (UIColor *)validGreen;
-+ (UIColor *)warningYellow;
-+ (UIColor *)errorRed;
-+ (UIColor *)alertYellowDark;
-+ (UIColor *)alertYellowLighter;
-+ (UIColor *)alertRedDarker;
++ (nonnull UIColor *)wordPressBlue;
++ (nonnull UIColor *)lightBlue;
++ (nonnull UIColor *)mediumBlue;
++ (nonnull UIColor *)darkBlue;
++ (nonnull UIColor *)grey;
++ (nonnull UIColor *)lightGrey;
++ (nonnull UIColor *)greyLighten30;
++ (nonnull UIColor *)greyLighten20;
++ (nonnull UIColor *)greyLighten10;
++ (nonnull UIColor *)greyDarken10;
++ (nonnull UIColor *)greyDarken20;
++ (nonnull UIColor *)greyDarken30;
++ (nonnull UIColor *)darkGrey;
++ (nonnull UIColor *)jazzyOrange;
++ (nonnull UIColor *)fireOrange;
++ (nonnull UIColor *)validGreen;
++ (nonnull UIColor *)warningYellow;
++ (nonnull UIColor *)errorRed;
++ (nonnull UIColor *)alertYellowDark;
++ (nonnull UIColor *)alertYellowLighter;
++ (nonnull UIColor *)alertRedDarker;
 
 // Misc
-+ (UIColor *)keyboardColor;
-+ (UIColor *)textFieldPlaceholderGrey;
-+ (UIColor *)tableViewActionColor;
++ (nonnull UIColor *)keyboardColor;
++ (nonnull UIColor *)textFieldPlaceholderGrey;
++ (nonnull UIColor *)tableViewActionColor;
 
 // Bar Button Styles
 + (UIBarButtonItemStyle)barButtonStyleForDone;
 + (UIBarButtonItemStyle)barButtonStyleForBordered;
-+ (void)setLeftBarButtonItemWithCorrectSpacing:(UIBarButtonItem *)barButtonItem forNavigationItem:(UINavigationItem *)navigationItem;
-+ (void)setRightBarButtonItemWithCorrectSpacing:(UIBarButtonItem *)barButtonItem forNavigationItem:(UINavigationItem *)navigationItem;
++ (void)setLeftBarButtonItemWithCorrectSpacing:(nonnull UIBarButtonItem *)barButtonItem forNavigationItem:(nonnull UINavigationItem *)navigationItem;
++ (void)setRightBarButtonItemWithCorrectSpacing:(nonnull UIBarButtonItem *)barButtonItem forNavigationItem:(nonnull UINavigationItem *)navigationItem;
 
 // View and TableView Helpers
-+ (void)configureColorsForView:(UIView *)view andTableView:(UITableView *)tableView;
-+ (void)configureColorsForView:(UIView *)view collectionView:(UICollectionView *)collectionView;
-+ (void)configureTableViewCell:(UITableViewCell *)cell;
-+ (void)configureTableViewSmallSubtitleCell:(UITableViewCell *)cell;
-+ (void)configureTableViewActionCell:(UITableViewCell *)cell;
-+ (void)configureTableViewDestructiveActionCell:(UITableViewCell *)cell;
-+ (void)configureTableViewTextCell:(WPTextFieldTableViewCell *)cell;
-+ (void)configureTableViewSectionHeader:(UIView *)header;
-+ (void)configureTableViewSectionFooter:(UIView *)footer;
++ (void)configureColorsForView:(nullable UIView *)view andTableView:(nullable UITableView *)tableView;
++ (void)configureColorsForView:(nullable UIView *)view collectionView:(nullable UICollectionView *)collectionView;
++ (void)configureTableViewCell:(nullable UITableViewCell *)cell;
++ (void)configureTableViewSmallSubtitleCell:(nullable UITableViewCell *)cell;
++ (void)configureTableViewActionCell:(nullable UITableViewCell *)cell;
++ (void)configureTableViewDestructiveActionCell:(nullable UITableViewCell *)cell;
++ (void)configureTableViewTextCell:(nullable WPTextFieldTableViewCell *)cell;
++ (void)configureTableViewSectionHeader:(nullable UIView *)header;
++ (void)configureTableViewSectionFooter:(nullable UIView *)footer;
 
 // Move to a feature category
-+ (UIColor *)buttonActionColor;
-+ (UIColor *)nuxFormText;
-+ (UIColor *)nuxFormPlaceholderText;
-+ (void)configureFollowButton:(UIButton *)followButton;
++ (nonnull UIColor *)buttonActionColor;
++ (nonnull UIColor *)nuxFormText;
++ (nonnull UIColor *)nuxFormPlaceholderText;
++ (void)configureFollowButton:(nullable UIButton *)followButton;
 
 // Deprecated Colors
-+ (UIColor *)baseLighterBlue;
-+ (UIColor *)baseDarkerBlue;
-+ (UIColor *)newKidOnTheBlockBlue;
-+ (UIColor *)midnightBlue;
-+ (UIColor *)bigEddieGrey;
-+ (UIColor *)littleEddieGrey;
-+ (UIColor *)whisperGrey;
-+ (UIColor *)allTAllShadeGrey;
-+ (UIColor *)readGrey;
-+ (UIColor *)itsEverywhereGrey;
-+ (UIColor *)darkAsNightGrey;
-+ (UIColor *)validationErrorRed;
++ (nonnull UIColor *)baseLighterBlue;
++ (nonnull UIColor *)baseDarkerBlue;
++ (nonnull UIColor *)newKidOnTheBlockBlue;
++ (nonnull UIColor *)midnightBlue;
++ (nonnull UIColor *)bigEddieGrey;
++ (nonnull UIColor *)littleEddieGrey;
++ (nonnull UIColor *)whisperGrey;
++ (nonnull UIColor *)allTAllShadeGrey;
++ (nonnull UIColor *)readGrey;
++ (nonnull UIColor *)itsEverywhereGrey;
++ (nonnull UIColor *)darkAsNightGrey;
++ (nonnull UIColor *)validationErrorRed;
 
 @end
