050faa65a22e64f36cfb04d36413aaa3dedc3e72
aerych <aerychj@gmail.com>
2018-11-13 17:30:59 -0600

    Text: Removes problematic markup from Gutenberg galleries.

--- WordPressSharedTests/RichContentFormatterTests.swift
+++ WordPressSharedTests/RichContentFormatterTests.swift
@@ -45,4 +45,16 @@ class RichContentFormatterTests: XCTestCase {
         let sanitizedStr = RichContentFormatter.removeTrailingBreakTags(styleStr)
         XCTAssertTrue(str == sanitizedStr, "The inline styles were not removed.")
     }
+
+
+    func testRemoveGutenbergGalleryListMarkup() {
+        let str = "Some text. <ul class=\"wp-block-gallery columns-3 is-cropped\"><li class=\"blocks-gallery-item\"><figure><img src=\"https://example.com/wp-content/uploads/2017/05/IMG_1364.jpg\" alt=\"\" data-id=\"103\" data-link=\"https://example.com/img_1364/\" class=\"wp-image-103\" srcset=\"https://example.com/wp-content/uploads/2017/05/IMG_1364.jpg 2048w, https://example.com/wp-content/uploads/2017/05/IMG_1364-300x225.jpg 300w, https://example.com/wp-content/uploads/2017/05/IMG_1364-768x576.jpg 768w, https://example.com/wp-content/uploads/2017/05/IMG_1364-1024x768.jpg 1024w, https://example.com/wp-content/uploads/2017/05/IMG_1364-1200x900.jpg 1200w\" sizes=\"(max-width: 2048px) 100vw, 2048px\" /></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://example.com/wp-content/uploads/2017/05/IMG_1215.jpg\" alt=\"\" data-id=\"102\" data-link=\"https://example.com/img_1215/\" class=\"wp-image-102\" srcset=\"https://example.com/wp-content/uploads/2017/05/IMG_1215.jpg 2048w, https://example.com/wp-content/uploads/2017/05/IMG_1215-300x225.jpg 300w, https://example.com/wp-content/uploads/2017/05/IMG_1215-768x576.jpg 768w, https://example.com/wp-content/uploads/2017/05/IMG_1215-1024x768.jpg 1024w, https://example.com/wp-content/uploads/2017/05/IMG_1215-1200x900.jpg 1200w\" sizes=\"(max-width: 2048px) 100vw, 2048px\" /></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://example.com/wp-content/uploads/2017/05/img_5918.jpg\" alt=\"\" data-id=\"101\" data-link=\"https://example.com/img_5918-jpg/\" class=\"wp-image-101\" srcset=\"https://example.com/wp-content/uploads/2017/05/img_5918.jpg 1000w, https://example.com/wp-content/uploads/2017/05/img_5918-225x300.jpg 225w, https://example.com/wp-content/uploads/2017/05/img_5918-768x1024.jpg 768w\" sizes=\"(max-width: 1000px) 100vw, 1000px\" /></figure></li></ul> Some text."
+        let sanitizedString = RichContentFormatter.formatGutenbergGallery(str) as NSString
+        var range = sanitizedString.range(of: "block-gallery")
+        XCTAssertTrue(range.location == NSNotFound)
+        range = sanitizedString.range(of: "blocks-gallery")
+        XCTAssertTrue(range.location == NSNotFound)
+        range = sanitizedString.range(of: "<figure>")
+        XCTAssertTrue(range.location == NSNotFound)
+    }
 }
