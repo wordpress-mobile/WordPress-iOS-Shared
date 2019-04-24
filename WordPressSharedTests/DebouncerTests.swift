d1e00f194a1147922db5f30eee7c7d17339291da
Diego Rey Mendez <diegoreymendez@gmail.com>
2019-04-24 17:26:33 -0300

    Adds a test for the Debouncer's cancel() method.

--- WordPressSharedTests/DebouncerTests.swift
+++ WordPressSharedTests/DebouncerTests.swift
@@ -42,4 +42,22 @@ class DebouncerTests: XCTestCase {
 
         wait(for: [debouncerHasRun], timeout: 0)
     }
+    
+    /// Tests that we can cancel the debouncer's operation.
+    ///
+    func testDebouncerCanBeCancelled() {
+        let debouncerDelay = 0.2
+        let testTimeout = debouncerDelay * 2
+        let debouncerHasRun = XCTestExpectation(description: "The debouncer's operation should be cancellable.")
+        debouncerHasRun.isInverted = true
+        
+        let debouncer = Debouncer(delay: debouncerDelay) {
+            debouncerHasRun.fulfill()
+        }
+        
+        debouncer.call()
+        debouncer.cancel()
+        
+        wait(for: [debouncerHasRun], timeout: testTimeout)
+    }
 }
