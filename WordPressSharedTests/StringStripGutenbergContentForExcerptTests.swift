e7b9c88b0778007415757b10645746c2625128fd
Diego Rey Mendez <diegoreymendez@gmail.com>
2020-01-29 16:19:14 -0300

    Adds a unit test and fixes the name of other 2 tests.

--- WordPressSharedTests/StringStripGutenbergContentForExcerptTests.swift
+++ WordPressSharedTests/StringStripGutenbergContentForExcerptTests.swift
@@ -3,7 +3,16 @@ import XCTest
 
 class StringStripGutenbergContentForExcerptTests: XCTestCase {
     
-    func testSummaryForContentWithGallery() {
+    func testStrippingGutenbergContentForExcerpt() {
+        let content = "<p>Some Content</p>"
+        let expectedSummary = "<p>Some Content</p>"
+        
+        let summary = content.strippingGutenbergContentForExcerpt()
+        
+        XCTAssertEqual(summary, expectedSummary)
+    }
+    
+    func testStrippingGutenbergContentForExcerptWithGallery() {
         let content = "<!-- wp:gallery {\"ids\":[2315,2309,2308]} --><figure class=\"wp-block-gallery columns-3 is-cropped\"><ul class=\"blocks-gallery-grid\"><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0005-1-1.jpg\" data-id=\"2315\" class=\"wp-image-2315\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0111-1-1.jpg\" data-id=\"2309\" class=\"wp-image-2309\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0004-1.jpg\" data-id=\"2308\" class=\"wp-image-2308\"/><figcaption class=\"blocks-gallery-item__caption\">Adsasdasdasd</figcaption></figure></li></ul></figure><!-- /wp:gallery --><p>Some Content</p>"
         let expectedSummary = "<p>Some Content</p>"
         
@@ -12,7 +21,7 @@ class StringStripGutenbergContentForExcerptTests: XCTestCase {
         XCTAssertEqual(summary, expectedSummary)
     }
     
-    func testSummaryForContentWithGallery2() {
+    func testStrippingGutenbergContentForExcerptWithGallery2() {
         let content = "<p>Before</p>\n<!-- wp:gallery {\"ids\":[2315,2309,2308]} --><figure class=\"wp-block-gallery columns-3 is-cropped\"><ul class=\"blocks-gallery-grid\"><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0005-1-1.jpg\" data-id=\"2315\" class=\"wp-image-2315\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0111-1-1.jpg\" data-id=\"2309\" class=\"wp-image-2309\"/><figcaption class=\"blocks-gallery-item__caption\">Asdasdasd</figcaption></figure></li><li class=\"blocks-gallery-item\"><figure><img src=\"https://diegotest4.files.wordpress.com/2020/01/img_0004-1.jpg\" data-id=\"2308\" class=\"wp-image-2308\"/><figcaption class=\"blocks-gallery-item__caption\">Adsasdasdasd</figcaption></figure></li></ul></figure><!-- /wp:gallery --><p>After</p>"
         let expectedSummary = "<p>Before</p>\n<p>After</p>"
         
