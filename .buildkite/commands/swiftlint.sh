58f9644f41dd3a7fe403245bafc3ca2fb3f9119a
Gio Lodi <gio@mokacoding.com>
2024-02-05 19:05:32 +1100

    Use `markdown` formatting to report SwiftLint violations

--- .buildkite/commands/swiftlint.sh
+++ .buildkite/commands/swiftlint.sh
@@ -3,21 +3,25 @@
 echo "--- :swift: Running SwiftLint"
 
 set +e
-SWIFTLINT_OUTPUT=$(swiftlint lint --quiet "$@" --reporter relative-path)
+SWIFTLINT_OUTPUT=$(swiftlint lint --quiet --reporter markdown "$@")
 SWIFTLINT_EXIT_STATUS=$?
 set -e
 
-WARNINGS=$(echo -e "$SWIFTLINT_OUTPUT" | awk -F': ' '/: warning:/ {printf "- `%s`: %s\n", $1, $4}')
-ERRORS=$(echo -e "$SWIFTLINT_OUTPUT" | awk -F': ' '/: error:/ {printf "- `%s`: %s\n", $1, $4}')
+WARNINGS=$(echo -e "$SWIFTLINT_OUTPUT" | awk '/:warning:/')
+ERRORS=$(echo -e "$SWIFTLINT_OUTPUT" | awk '/:error:/')
+
+# The SwiftLint Markdown output has the table formatting in the first two lines, so we print them, too.
 
 if [ -n "$WARNINGS" ]; then
   echo "$WARNINGS"
-  printf "**SwiftLint Warnings**\n%b" "$WARNINGS" | buildkite-agent annotate --style 'warning'
+  printf "**SwiftLint Warnings**\n%b\n%b\n" "$(echo -e "$SWIFTLINT_OUTPUT" | head -n 2)" "$WARNINGS" \
+    | buildkite-agent annotate --style 'warning'
 fi
 
 if [ -n "$ERRORS" ]; then
   echo "$ERRORS"
-  printf "**SwiftLint Errors**\n%b" "$ERRORS" | buildkite-agent annotate --style 'error'
+  printf "**SwiftLint Errors**\n%b\n%b\n" "$(echo -e "$SWIFTLINT_OUTPUT" | head -n 2)" "$ERRORS" \
+    | buildkite-agent annotate --style 'error'
 fi
 
 exit $SWIFTLINT_EXIT_STATUS
